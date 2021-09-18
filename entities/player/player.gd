2d15b937584c8dafe38088156dd26a9124134a72
NathanX-S <nathanstgeorge7@gmail.com>
2021-09-18 04:25:59 -0400

    player: more work on player, use delta time

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,34 +1,31 @@
 extends KinematicBody2D
 
-var movespeed : int = 250 #250 pixels per press, i think
-var score = 0 #score = time? 
+var movespeed := 250 #250 pixels per press, i think
+var score := 0 #score = time? 
 
-var velocity : Vector2 = Vector2() #movement
-var facingDir : Vector2 = Vector2() #this is so that the character will continue facing in that direction where you left them */
-var vel : Vector2 = Vector2() #character directions
+var velocity := Vector2.ZERO
+var facingDir := Vector2() #this is so that the character will continue facing in that direction where you left them */
+var vel := Vector2() #character directions
 
 
-onready var rayCast = get_node("RayCast2d")
+onready var rayCast = get_node("RayCast2D")
+
+export var speed = 200  # speed in pixels/sec
+
+func get_input():
+	velocity = Vector2.ZERO # reset velocity every tick
+	if Input.is_action_pressed('move_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('move_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('move_up'):
+		velocity.y -= 1
+	# Make sure diagonal movement isn't faster
+
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-	
-	vel = Vector2()
-	
-	#key presses
-	if Input.is_action_pressed("move_up"):
-		vel.y -= 1
-		facingDir = Vector2(0, -1)
-	if Input.is_action_pressed("move_down"):
-		vel.y += 1
-		facingDir = Vector2(0, 1)
-	if Input.is_action_pressed("move_left"):
-		vel.x -= 1
-		facingDir = Vector2(-1, 0)
-	if Input.is_action_pressed("move_right"):
-		vel.x += 1
-		facingDir = Vector2(1,0)
-		
-	vel = vel.normalized() #this is so that if the character goes diagonally *ex upper right, they won't speed up like bunny hopping
-	
-	move_and_slide(vel*movespeed) 
-	 
+	get_input() # seperate this into its own function
+	velocity = move_and_slide(velocity * delta) 
