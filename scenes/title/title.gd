748011534db171f5ba6e09eb3bb066ea47bcbbef
NathanX-S <nathanstgeorge7@gmail.com>
2021-09-19 04:42:17 -0400

    title_screen: implemented initial version

---
+++ scenes/title/title.gd
@@ -0,0 +1,24 @@
+extends Panel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var file = File.new()
+	file.open("res://version.txt", File.READ)
+	var ver = file.get_as_text()
+	file.close()
+	find_node("Version").set_text(ver)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://scenes/tutorial/tut.tscn")
