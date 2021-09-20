b74262693ea809eaa3d2a442aeb09acec0bf613e
NathanX-S <nathanstgeorge7@gmail.com>
2021-09-20 07:12:57 -0400

    hud: timer :D

--- scenes/global.gd
+++ scenes/global.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var current_scene = null
+var run_time := 0.00
+var hud
 
 func _ready():
 	var root = get_tree().get_root()
@@ -13,9 +15,10 @@ func _d_enter_level(path):
 	current_scene.free() # nuke current scene :(
 		
 	var new_scene = load(path).instance()
-	var hud = load("res://scenes/hud.tscn").instance()
+	hud = load("res://scenes/hud.tscn").instance()
 	new_scene.add_child(hud)
 	current_scene = new_scene
 	
 	get_tree().get_root().add_child(current_scene)
 	get_tree().set_current_scene(current_scene) # make compatiable with godot API
+	run_time = 0.00
---
+++ scenes/hud.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var timer
+onready var global_script = get_node("/root/global")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer = self.get_node("TimerLabel")
+
+func _process(delta):
+	var time_text = ""
+	var run_time = global_script.run_time
+	var seconds = fmod(run_time, 60.0)
+	run_time /= 60
+	var minutes = fmod(run_time, 60.0)
+	print(seconds)
+	print(minutes)
+	if minutes >= 1:
+		time_text = str(floor(minutes)) + ":"
+	time_text = time_text + str(stepify(seconds, 0.01))
+	timer.set_text(time_text)
+	global_script.run_time += delta
