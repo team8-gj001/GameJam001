4db6481f35a065c402ceed4573a1d64de9b29286
NathanX-S <nathanstgeorge7@gmail.com>
2021-09-27 11:18:49 -0400

    Update guard.gd

--- entities/enemies/guard.gd
+++ entities/enemies/guard.gd
@@ -14,17 +14,21 @@ var t = 0 # Active time along the apth
 var pathLength = 0 # Length of the path
 
 func _ready():
-	print(path)
 	pathLength = path.get_curve().get_baked_length()
 
 func _process(_delta):
 	if (t > traverseTime):
 		t = 0
 	t += _delta
-	print(t)
 	pathFollow.set_offset((t/traverseTime) * pathLength)
 
 func _physics_process(_delta):
-	if rcTop.is_colliding() or rcMid.is_colliding() or rcBot.is_colliding():
-		if rcTop.get_collider().name == "Player" or rcMid.get_collider().name == "Player" or rcBot.get_collider().name == "Player":
-				get_node("/root/global").kill_player()
+	if rcTop.is_colliding() and rcTop.get_collider() != null:
+		if rcTop.get_collider().name == "Player":
+			get_node("/root/global").kill_player()
+	if rcMid.is_colliding() and rcMid.get_collider() != null:
+		if rcMid.get_collider().name == "Player":
+			get_node("/root/global").kill_player()
+	if rcBot.is_colliding() and rcBot.get_collider() != null:
+		if rcBot.get_collider().name == "Player":
+			get_node("/root/global").kill_player()
