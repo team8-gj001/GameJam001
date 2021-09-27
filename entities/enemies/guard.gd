92b1941488b90a64598aa0b7c4248a83435d027a
NathanX-S <nathanstgeorge7@gmail.com>
2021-09-26 21:20:20 -0400

    entities: guard work

--- entities/enemies/guard.gd
+++ entities/enemies/guard.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-var score := 0 #score = time? 
-
 var velocity := Vector2.ZERO
 var facingDir := Vector2() #this is so that the character will continue facing in that direction where you left them */
 var vel := Vector2() #character directions
@@ -15,14 +13,6 @@ export var speed = 200  # speed in pixels/sec
 
 func get_input():
 	velocity = Vector2.ZERO # reset velocity every tick
-	if Input.is_action_pressed('move_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('move_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('move_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('move_up'):
-		velocity.y -= 1
 	# Make sure diagonal movement isn't faster
 
 	velocity = velocity.normalized() * speed
@@ -32,4 +22,6 @@ func get_input():
 
 func _physics_process(_delta):
 	get_input() # seperate this into its own function
+	if rayCast.is_colliding():
+			print(rayCast.get_collider().name)
 	var movement = move_and_slide(velocity)
